fe0c9727c5143ef9161ba62aa81265a049b4c666
Isabelle Rose Lipton <belle_lipton@harvard.edu>
2023-08-17 18:57:41 -0400

    Update pivot.R

--- national-survey/steps/pivot/pivot.R
+++ national-survey/steps/pivot/pivot.R
@@ -4,7 +4,7 @@ library(tidyverse)
 #Import the Mexican national survey data
 national_survey_data <- read_csv(file = "https://raw.githubusercontent.com/HarvardMapCollection/modalities-cleaning/main/original/tabular/ENPOL2021_2_3.csv")
 
-#Let's just keep the unique ID and the column of interest
+#Let's pare the source data's 184 columns (😅) down to the only one we want to map
 national_survey_data <- national_survey_data %>% select ("P3_6") 
 
 #Rename the confusing column header "P3_6" to something we understand
