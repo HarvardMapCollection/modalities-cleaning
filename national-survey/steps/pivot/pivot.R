d415825a0f296c5b6bac526e95decf9b240764fa
Isabelle Rose Lipton <belle_lipton@harvard.edu>
2023-08-17 18:55:48 -0400

    Update pivot.R

--- national-survey/steps/pivot/pivot.R
+++ national-survey/steps/pivot/pivot.R
@@ -7,11 +7,15 @@ national_survey_data <- read_csv(file = "https://raw.githubusercontent.com/Harva
 #Let's just keep the unique ID and the column of interest
 national_survey_data <- national_survey_data %>% select ("P3_6") 
 
-
+#Rename the confusing column header "P3_6" to something we understand
+#about the location of where crimes occurred, "CRIME_LOC"
 national_survey_data <- national_survey_data %>%
   rename("CRIME_LOC" = "P3_6")
 
-
+#group_by groups the records by the recurring values within
+#With summarise(), we can specify the summary statistics we want
+#In this case, we want to create a new column titled "COUNT"
+#And the "=n" specifies that we want to count, rather than sum or mean, etc.
 national_survey_data <- national_survey_data %>% group_by(CRIME_LOC) %>%
   summarise(COUNT=n())
 
